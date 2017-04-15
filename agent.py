ea9ed2e40e3fa97cc202df5f48f124ddddfdcb78
Berker Sönmez <brkrsnmz@gmail.com>
2017-04-15 18:59:36 +0300

    Implementation of custom movement, a* pathfinding

--- agent.py
+++ agent.py
@@ -20,14 +20,12 @@ class Entity:
         elif typ == "BARREL":
             self.rum = arg1  # type: int
         elif typ == "CANNONBALL":
-            self.owner = arg1  #type: int
-            self.turns = arg2  #type: int
+            self.owner = arg1  # type: int
+            self.turns = arg2  # type: int
 
-class Global:
-    me_ship_cnt = 0
-    entity_cnt = 0
-    entities = []  # type: List[Entity]
-    ships = []  # type: List[Ship]
+    def hex(self):
+        return Hex(self.x, self.y)
+
 
 class Ship:
     def __init__(self, entity):
@@ -42,23 +40,30 @@ class Ship:
 
     def think(self):
         self.cooldown()
-        self.move = "WAIT"
+        self.action = "WAIT"
 
         enemy_ships = [x for x in Global.entities if x.type == "SHIP" and not x.me]
-        enemy_ship = min(enemy_ships, key= lambda ship: distance(self.entity, ship))  # type: Entity
+        enemy_ship = min(enemy_ships, key=lambda ship: distance(self.entity, ship)) \
+            if len(enemy_ships) > 0 else None  # type: Entity
         barrels = [x for x in Global.entities if x.type == "BARREL"]
         mines = [x for x in Global.entities if x.type == "MINE"]
         cannonballs = [x for x in Global.entities if x.type == "CANNONBALL"]
 
-        if self.cannon_cd == 0 and enemy_ship is not None and distance(self.entity, enemy_ship) < 10:
-            self.target = enemy_ship
-            self.move = "FIRE"
-            return
+        # Fire at enemy
+        # if self.cannon_cd == 0 and enemy_ship is not None and distance(self.entity, enemy_ship) < 10:
+        #     self.target = enemy_ship
+        #     self.move = "FIRE"
+        #     return
 
-        if len(barrels) > 0:
-            barrels = heapq.nsmallest(2, barrels, key=lambda barrel: distance(self.entity, barrel))
-            self.target = max(barrels, key=lambda barrel: barrel.rum)
-            self.move = "MOVE"
+        # Hunt for barrels
+        while len(barrels) > 0:
+            barrels = heapq.nsmallest(1, barrels, key=lambda barrel: distance(self.entity, barrel))
+            target = max(barrels, key=lambda barrel: barrel.rum)
+            path = find_path(self.entity.hex(), target.hex(), self)
+            if path is None:
+                barrels.remove(target)
+                continue
+            self.move(path)
             return
 
         # self.target = Entity("99", "-", self.entity.x + random.randint(0,1), self.entity.y + random.randint(0,1), "", "", "", "")
@@ -73,16 +78,121 @@ class Ship:
         self.mine_cd = self.mine_cd - 1 if self.mine_cd > 0 else 0
 
     def act(self):
-        if self.move == "WAIT":
+        if self.action == "WAIT":
             print("WAIT")
-        elif self.move == "FIRE":
+        elif self.action == "FIRE":
             self.cannon_cd = 2
             print("FIRE " + str(self.target.x) + " " + str(self.target.y))
-        elif self.move == "MOVE":
+        elif self.action == "MOVE":
             print("MOVE " + str(self.target.x) + " " + str(self.target.y))
-        elif self.move == "MINE":
+        elif self.action == "MINE":
             self.mine_cd = 5
             print("MINE")
+        elif self.action == "PORT":
+            print("PORT")
+        elif self.action == "STARBOARD":
+            print("STARBOARD")
+        elif self.action == "FASTER":
+            print("FASTER")
+        elif self.action == "SLOWER":
+            print("SLOWER")
+        else:
+            print("WAIT")
+
+    def move(self, path):
+        self.action = "WAIT"
+        next_hex = path[self.entity.hex()]
+        next_dir = direction_between(self.entity.hex(), next_hex)
+        second_hex = path[next_hex] if next_hex in path else None
+        second_dir = direction_between(next_hex, second_hex) if second_hex is not None else None
+        third_hex = path[second_hex] if second_hex is not None and second_hex in path else None
+        third_dir = direction_between(second_hex, third_hex) if third_hex is not None else None
+        if self.entity.spd == 0:
+            # If we are stationary, turn if next dir is different, else get faster
+            if next_dir != self.entity.ori:
+                self.action = "PORT" if required_rotation(self.entity.ori, next_dir) == 1 else "STARBOARD"
+            else:
+                self.action = "FASTER"
+        elif self.entity.spd == 1:
+            # If our speed is one:
+            # - Slow down if need to turn immediately
+            # - Turn if need to turn with distance 1
+            # - Wait if need to turn with distance 2
+            # - Get faster if no need to turn
+            if next_dir != self.entity.ori:
+                print("NEXT_DIR:" + str(next_dir) + " NOW_ORI:" + str(self.entity.ori), file=sys.stderr)
+                self.action = "SLOWER"
+            elif second_dir is not None and second_dir != next_dir:
+                self.action = "PORT" if required_rotation(next_dir, second_dir) == 1 else "STARBOARD"
+            elif third_dir is not None and third_dir != second_dir:
+                self.action = "WAIT"
+            elif third_dir is not None:
+                self.action = "FASTER"
+        elif self.entity.spd == 2:
+            # If our speed is two:
+            # - Slow down if need to turn immediately or with distance 1
+            # - Turn if need to turn with distance 2
+            # - Wait if no need to turn
+            if next_dir != self.entity.ori or (second_dir is not None and second_dir != next_dir):
+                self.action = "SLOWER"
+            elif third_dir is not None and third_dir != second_dir:
+                self.action = "PORT" if required_rotation(second_dir, third_dir) == 1 else "STARBOARD"
+            elif third_dir is not None:
+                self.action = "WAIT"
+            else:
+                self.action = "SLOWER"
+        return
+
+
+# region HEX GRID LOGIC
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+class Hex:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def neighbor(self, direction):
+        parity = abs(self.y) % 2
+        dirn = Global.dir_to_hex[parity][direction]
+        return Hex(self.x + dirn.x, self.y + dirn.y)
+
+    def neighbors(self):
+        return [nbr for nbr in [self.neighbor(dirn) for dirn in range(6)] if in_grid(Global.grid, nbr)]
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __lt__(self, other):
+        return self.x < other.x or self.y < other.y
+
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+
+def in_grid(grid, target):
+    return len(grid) > target.x and len(grid[0]) > target.y
+
 
 def distance(target1, target2):
     """
@@ -93,14 +203,128 @@ def distance(target1, target2):
     x2, y2, z2 = offset_to_cube(target2.x, target2.y)
     return cube_distance(x1, y1, z1, x2, y2, z2)
 
+
+def cost(from_hex, to_hex, came_from, current_ori):
+    dist = distance(from_hex, to_hex)
+    if came_from[from_hex] is None:
+        prev_dir = current_ori
+    else:
+        prev_dir = direction_between(came_from[from_hex], from_hex)
+    now_dir = direction_between(from_hex, to_hex)
+    turn_cost = turning_cost(prev_dir, now_dir)
+    return dist + turn_cost
+
+
+def direction_between(from_hex, to_hex):
+    difx = to_hex.x - from_hex.x
+    dify = to_hex.y - from_hex.y
+    return Global.hex_to_dir[from_hex.y % 2][(difx, dify)]
+
+
+def turning_cost(from_dir, to_dir):
+    cost1 = abs(from_dir - to_dir)
+    if cost1 <= 3:
+        return cost1
+    return 6 - cost1
+
+
+def required_rotation(from_dir, to_dir):
+    # Returns 0 for right, 1 for left
+    diff_dir = to_dir - from_dir
+    if diff_dir < 3:
+        return 1 if to_dir > from_dir else 0
+    else:
+        return 0 if to_dir > from_dir else 1
+
+
 def offset_to_cube(x, y):
-    ox = y - (x - (x % 2)) / 2
-    oz = x
+    ox = x - (y - (abs(y) % 2)) / 2
+    oz = y
     oy = -ox - oz
-    return ox, oz, oy
+    return ox, oy, oz
+
 
 def cube_distance(x1, y1, z1, x2, y2, z2):
     return (abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)) / 2
+
+
+def heuristic(a, b):
+    return distance(a, b)
+
+
+def find_path(beg, tar, ship):
+    """
+    A* path-finding implementation
+    :type beg: Hex
+    :type tar: Hex
+    """
+    frontier = PriorityQueue()
+    frontier.put(beg, 0)
+    came_from = {beg: None}
+    cost_so_far = {beg: 0}
+    found = False
+
+    while not frontier.empty():
+        cur = frontier.get()
+        if cur == tar:
+            found = True
+            break
+        for nxt in [neighbor for neighbor in cur.neighbors() if not is_obstacle(neighbor)]:
+            new_cost = cost_so_far[cur] + cost(cur, nxt, came_from, ship.entity.ori)
+            if nxt not in cost_so_far or new_cost < cost_so_far[nxt]:
+                cost_so_far[nxt] = new_cost
+                priority = new_cost + heuristic(tar, nxt)
+                frontier.put(nxt, priority)
+                came_from[nxt] = cur
+    if not found:
+        return None
+    # Construct path
+    path = {}
+    cur = tar
+    while cur != beg:
+        path[came_from[cur]] = cur
+        cur = came_from[cur]
+    if ship.entity.id == 0:
+        print("TARGET: " + str(tar), file=sys.stderr)
+        print("---PATH----", file=sys.stderr)
+        for key, value in path.items():
+            print(str(key) + " -> " + str(value), file=sys.stderr)
+        print("-----------", file=sys.stderr)
+    return path
+
+
+def is_obstacle(target):
+    """
+    :type target: Hex
+    """
+    entity = Global.grid[target.x][target.y]
+    if entity is None:
+        return False
+    if entity.type == "MINE":
+        return True
+    for neighbor in target.neighbors():
+        if Global.grid[neighbor.x][neighbor.y] is not None and Global.grid[neighbor.x][neighbor.y].type == "MINE":
+            return True
+    return False
+
+
+# endregion
+
+class Global:
+    me_ship_cnt = 0
+    entity_cnt = 0
+    entities = []  # type: List[Entity]
+    ships = []  # type: List[Ship]
+    grid = [[None for x in range(21)] for y in range(23)]  # type: List[List[Entity]]
+    dir_to_hex = [
+        [Hex(+1, 0), Hex(0, -1), Hex(-1, -1),
+         Hex(-1, 0), Hex(-1, +1), Hex(0, +1)],
+        [Hex(+1, 0), Hex(+1, -1), Hex(0, -1),
+         Hex(-1, 0), Hex(0, +1), Hex(+1, +1)]
+    ]
+    hex_to_dir = [{(+1, 0):0, (0, -1):1, (-1, -1):2, (-1, 0):3, (-1, +1):4, (0, +1):5},
+                  {(+1, 0):0, (+1, -1):1, (0, -1):2, (-1, 0):3, (0, +1):4, (+1, +1):5}]
+
 
 def play():
     # game loop
@@ -116,6 +340,7 @@ def get_inputs():
     Global.entity_cnt = int(input())  # the number of entities (e.g. ships, mines or cannonballs)
     entities = []
     ships = []
+    Global.grid = [[None for _ in range(21)] for _ in range(23)]
     for i in range(Global.entity_cnt):
         entity_id, entity_type, x, y, arg_1, arg_2, arg_3, arg_4 = input().split()
         entity_id = int(entity_id)
@@ -127,6 +352,7 @@ def get_inputs():
         arg_4 = int(arg_4)
         entity = Entity(entity_id, entity_type, x, y, arg_1, arg_2, arg_3, arg_4)
         entities.append(entity)
+        Global.grid[x][y] = entity
         if entity.type == "SHIP" and entity.me:
             ships.append(Ship(entity))
     Global.entities = entities
@@ -135,6 +361,7 @@ def get_inputs():
 
 def main():
     play()
+
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -2,15 +2,22 @@ from unittest import mock, TestCase
 
 import sys
 
-from agent import agent
+from agent import play
+
+
+referee = ['1', '2',
+           '1 SHIP 13 2 3 1 5 1',
+           '2 BARREL 13 6 20 0 0 0']
+referee = referee * 10
 
 
 class AgentUnitTests(TestCase):
 
-    @mock.patch('builtins.input', side_effect=['1', '4', 'Bob'])
+    @mock.patch('builtins.input', side_effect=referee)
     def test_wood2(self, input):
-        agent()
-        output = sys.stdout.getline().strip()
-        assert output == "You are too young"
+        play()
+        assert sys.stdout.getline().strip() == "MOVE 4 5"
         # Check the output after "13" and "Bob" are entered as well!
-        assert sys.stdout.getline().strip() == "Welcome!"
+        assert sys.stdout.getline().strip() == "MOVE 5 5"
+        assert sys.stdout.getline().strip() == "MOVE 5 6"
+        assert sys.stdout.getline().strip() == "MOVE 6 6"
